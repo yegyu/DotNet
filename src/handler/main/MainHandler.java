63473930e521e2b43bf2287b5edf548eedbd77f2
wlwhsldg <wlwhsldg@gmail.com>
2019-08-16 21:45:26 +0900

    소스트리 한글깨짐 테스트

--- src/handler/main/MainHandler.java
+++ src/handler/main/MainHandler.java
@@ -43,7 +43,8 @@ public class MainHandler implements CommandHandler {
 	@RequestMapping(value = {"/a", "/search" }, method = RequestMethod.POST, produces = "application/json;UTF-8")
 	@ResponseBody
 	public Map<String, Object> alignAjax(HttpServletRequest request) throws Exception {
-
+// 동근아 
+// 한글깨짐 테스트
 //		System.out.println("alignAjax �븿�닔 �뱾�뼱�샂");
 
 //		SurveyDBBean surveyDao = new SurveyDBBean();
